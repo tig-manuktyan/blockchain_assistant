9663f45c5b9df5b2100a006549f5e31ace370871
unknown <tigran.manukyan.2002@gmail.com>
2025-02-06 10:04:02 +0400

    changes

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -13,7 +13,7 @@ export class BotService {
   constructor(
     private readonly cryptoService: CryptoService,
     private readonly openAiService: OpenAiService,
-  ) {}
+  ) { }
 
   @Start()
   async startCommand(@Ctx() ctx: Context) {
@@ -23,15 +23,16 @@ export class BotService {
   @Hears(/.*/)
   async handleMessage(@Ctx() ctx: Context) {
     const input = ctx?.message?.['text']?.trim();
-  
+
     if (!input) {
       return await ctx.reply('Пожалуйста, введите текст.');
     }
-  
+
     try {
       const answer = await this.openAiService.ask(input) as string;
       const condition = this.parseAnswer(answer);
-  
+      console.log(condition,"conditioncondition");
+      
       if (condition?.type === 'crypto') {
         return await this.handleCryptoRequest(ctx, condition);
       } else {
@@ -42,7 +43,7 @@ export class BotService {
       await ctx.reply('Извините, произошла ошибка при обработке вашего запроса.');
     }
   }
-  
+
   private parseAnswer(answer: string) {
     try {
       return JSON.parse(answer);
@@ -51,33 +52,35 @@ export class BotService {
       return null;
     }
   }
-  
+
   private async handleCryptoRequest(ctx: Context, condition: any) {
     try {
       const coin = await this.cryptoService.findCoin(condition?.symbol ?? '');
       if (coin) {
         const price = await this.cryptoService.getPriceById(coin.id);
         if (price !== null) {
-          await ctx.reply(
+          await ctx.reply(condition.lang == "en" ?
+            `Current price for"${coin.name}" (${coin.symbol.toUpperCase()}): ${price} USD. Something else?`
+            :
             `Текущая цена для "${coin.name}" (${coin.symbol.toUpperCase()}): ${price} USD. Что-то еще?`,
           );
         } else {
-          await ctx.reply('Не удалось получить цену, попробуйте позже.');
+          await ctx.reply(condition.lang == "en" ? "Unable to get price, please try again later." : 'Не удалось получить цену, попробуйте позже.');
         }
       }
     } catch (error) {
       this.logger.error(`Ошибка при обработке крипто-запроса:`, error);
-      await ctx.reply('Не удалось обработать запрос о криптовалюте.');
+      await ctx.reply(condition.lang == "en" ? "Unable to process cryptocurrency request." : 'Не удалось обработать запрос о криптовалюте.');
     }
   }
-  
+
   private async handleGeneralRequest(ctx: Context, input: string) {
     try {
       const askCorrect = await this.openAiService.askCorrect(input) as string;
-       await ctx.reply(askCorrect);
+      await ctx.reply(askCorrect);
     } catch (error) {
       this.logger.error('Ошибка при обработке запроса:', error);
-      await ctx.reply('Не удалось получить ответ, попробуйте позже.');
+      // await ctx.reply('Не удалось получить ответ, попробуйте позже.');
     }
   }
 
